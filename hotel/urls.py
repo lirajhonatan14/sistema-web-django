3cd434946f72f31abfb434f7f6643d9dbffc8f5d
Jhonatan Oliveira <lirajhonatan14@gmail.com>
2023-06-16 13:04:02 -0300

    Atualização 2.5

--- hotel/urls.py
+++ hotel/urls.py
@@ -12,10 +12,10 @@ urlpatterns = [
         path('lista_reservas/', views.reserva_list, name='lista_reserva'),
         path('lista_reservasday/', views.reservaday_list, name='lista_reservaday'),
         path('procurar_reserva/', views.puxar_reserva, name='puxar_reserva'),
-        path('mostrar_reserva/', views.mostrar_reserva, name='mostrar_reserva'),
+        path('mostrar_reserva/<int:num_reserva>', views.mostrar_reserva, name='mostrar_reserva'),
         path('historico_reservas/', views.proc_reserva, name='historico_reserva'),
         path('procurar_reservaday/', views.puxar_reservaday, name='puxar_reservaday'),
-        path('mostrar_reservaday/', views.mostrar_reservaday, name='mostrar_reservaday'),
+        path('mostrar_reservaday/<int:num_reserva>  ', views.mostrar_reservaday, name='mostrar_reservaday'),
         path('renovar_pacote/', views.pacote, name='pacote'),
         path('pacote_renovado/', views.pacote_reservado, name='pacote_reservado'),
         
